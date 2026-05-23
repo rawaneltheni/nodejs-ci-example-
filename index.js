3c5c427da876da60882275ca397ee8291d6cab73
rawaneltheni <rawaneltheni@gmail.com>
2026-05-23 15:56:43 +0200

    ci: Add Docker build and push to workflow

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const http = require('http');
+const port = 3000;
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello from Dockerized Node.js App!\n');
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
